38ce81a72e811f50389de3b6e0b16a8bd442d97a
Kazurov Kostya <kazurovzxc@mail.ru>
2025-05-14 01:21:17 +0200

    test

--- src/test/java/shareit/ShareItTests.java
+++ src/test/java/shareit/ShareItTests.java
@@ -2,8 +2,9 @@ package shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.ShareItApp;
 
-@SpringBootTest
+@SpringBootTest(classes = ShareItApp.class)
 class ShareItTests {
 
 	@Test
