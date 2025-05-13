0ee5d94e7724d845cf8d51bc93a9b621abc85672
Kazurov Kostya <kazurovzxc@mail.ru>
2025-05-14 00:59:22 +0200

    item

---
+++ src/main/java/ru/yandex/practicum/item/Item.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.item;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import ru.yandex.practicum.request.ItemRequest;
+import ru.yandex.practicum.user.User;
+
+@Data
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @NotNull
+    @Size(min = 10, max = 1000)
+    private String description;
+
+    private boolean available = true;
+
+    @ManyToOne
+    @JoinColumn(name = "owner")
+    private User owner;
+
+    @OneToOne
+    @JoinColumn(name = "request")
+    private ItemRequest request;
+}
