6e852a742456883152959a63992db29e6c5bfca9
Kazurov Kostya <kazurovzxc@mail.ru>
2025-05-14 02:15:21 +0200

    checkStyle

--- src/main/java/ru/yandex/practicum/booking/Booking.java
+++ src/main/java/ru/yandex/practicum/booking/Booking.java
@@ -19,11 +19,11 @@ public class Booking {
 
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime StartDate;
+    private LocalDateTime startDate;
 
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime EndDate;
+    private LocalDateTime endDate;
 
     @ManyToOne
     @JoinColumn(name = "item_id", nullable = false)
--- src/main/java/ru/yandex/practicum/request/ItemRequestController.java
+++ src/main/java/ru/yandex/practicum/request/ItemRequestController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.request;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
