753d8373e8f309440601f657ca17587750236558
Kazurov Kostya <kazurovzxc@mail.ru>
2025-06-03 00:41:35 +0200

    ФЗ-16 checkStyle

--- server/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -19,44 +19,27 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
-    public ResponseEntity<BookingDto> addBooking(@RequestBody
-                                                 BookingCreateDto booking,
-                                                 @RequestHeader("X-Sharer-User-Id")
-                                                 Long userId) {
+    public ResponseEntity<BookingDto> addBooking(@RequestBody BookingCreateDto booking, @RequestHeader("X-Sharer-User-Id") Long userId) {
         return ResponseEntity.ok(bookingService.createBooking(booking, userId));
     }
 
     @PatchMapping("{bookingId}")
-    public ResponseEntity<BookingDto> bookingConfirmation(@PathVariable
-                                                          Long bookingId,
-                                                          @RequestHeader("X-Sharer-User-Id")
-                                                          Long userId,
-                                                          @RequestBody
-                                                          Boolean approved) {
+    public ResponseEntity<BookingDto> bookingConfirmation(@PathVariable Long bookingId, @RequestHeader("X-Sharer-User-Id") Long userId, @RequestBody Boolean approved) {
         return ResponseEntity.ok(bookingService.bookingConfirmation(bookingId, userId, approved));
     }
 
     @GetMapping("{bookingId}")
-    public ResponseEntity<BookingDto> getBookingById(@PathVariable("bookingId")
-                                                     Long bookingId,
-                                                     @RequestHeader("X-Sharer-User-Id")
-                                                     Long userId) {
+    public ResponseEntity<BookingDto> getBookingById(@PathVariable("bookingId") Long bookingId, @RequestHeader("X-Sharer-User-Id") Long userId) {
         return ResponseEntity.ok(bookingService.getBookingById(bookingId, userId));
     }
 
     @GetMapping
-    public ResponseEntity<List<BookingDto>> getAllUsersBookings(@RequestParam(defaultValue = "ALL")
-                                                                String state,
-                                                                @RequestHeader("X-Sharer-User-Id")
-                                                                Long userId) {
+    public ResponseEntity<List<BookingDto>> getAllUsersBookings(@RequestParam(defaultValue = "ALL") String state, @RequestHeader("X-Sharer-User-Id") Long userId) {
         return ResponseEntity.ok(bookingService.getAllUsersBookings(state, userId));
     }
 
     @GetMapping("/owner")
-    public ResponseEntity<List<BookingDto>> getAllBookingsForAllUsersItems(@RequestParam(defaultValue = "ALL")
-                                                                           String state,
-                                                                           @RequestHeader("X-Sharer-User-Id")
-                                                                           Long userId) {
+    public ResponseEntity<List<BookingDto>> getAllBookingsForAllUsersItems(@RequestParam(defaultValue = "ALL") String state, @RequestHeader("X-Sharer-User-Id") Long userId) {
         return ResponseEntity.ok(bookingService.getAllBookingsForAllUsersItems(state, userId));
     }
 }
--- server/src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -7,23 +7,12 @@ import ru.practicum.shareit.booking.model.Booking;
 public class BookingMapper {
 
     public static Booking toBooking(BookingCreateDto bookingCreateDto) {
-        return Booking.builder()
-                .start(bookingCreateDto.getStart())
-                .ending(bookingCreateDto.getEnd())
-                .status(BookingStatus.WAITING)
-                .build();
+        return Booking.builder().start(bookingCreateDto.getStart()).ending(bookingCreateDto.getEnd()).status(BookingStatus.WAITING).build();
     }
 
 
     public static BookingDto toDto(Booking booking) {
-        return BookingDto.builder()
-                .id(booking.getId())
-                .start(booking.getStart())
-                .end(booking.getEnding())
-                .item(booking.getItem())
-                .booker(booking.getBooker())
-                .status(booking.getStatus())
-                .build();
+        return BookingDto.builder().id(booking.getId()).start(booking.getStart()).end(booking.getEnding()).item(booking.getItem()).booker(booking.getBooker()).status(booking.getStatus()).build();
     }
 }
 
