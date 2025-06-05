d8ef4ab19c8106dba200b09573434a6a2fc466a3
Kazurov Kostya <kazurovzxc@mail.ru>
2025-06-05 21:21:29 +0200

    exceptionTest

--- server/src/test/java/ru/practicum/shareit/request/RequestServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/request/RequestServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.request.dto.RequestCreateDto;
 import ru.practicum.shareit.request.dto.RequestDto;
 import ru.practicum.shareit.request.dto.RequestShortDto;
